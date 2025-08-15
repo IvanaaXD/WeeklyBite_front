9564cda40105b82b895ae7f73cb78a62b031de9a
Ivana_R <ivanar1357@gmail.com>
2025-08-15 16:58:10 +0200

    fix css for nav-bar-au

--- src/app/layout/nav-bar-au/nav-bar-au.component.ts
+++ src/app/layout/nav-bar-au/nav-bar-au.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-
+import { AuthService } from '../../infrastructure/auth/auth.service';
 
 @Component({
   selector: 'app-nav-bar-au',
@@ -8,5 +8,18 @@ import { Router } from '@angular/router';
   styleUrls: ['./nav-bar-au.component.css']
 })
 export class NavBarAuComponent {
+
+  role: string | null = null;
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  ngOnInit(): void {
+      this.role = this.authService.getRole();
+  }
+
+  logOut(): void {
+    this.authService.logout()
+    this.router.navigate(['/login']);
+  }
 
 }
